8f4189915da5204c82b7d9aa6c5bd556a524886a
flu0r1ne <flu0r1ne@flu0r1ne.net>
2022-02-21 20:49:41 -0600

    Fix missing brace, add stream assertions

--- tests/include/assertions.h
+++ tests/include/assertions.h
@@ -92,6 +92,12 @@ void tdbg_clear_output(const char * file, unsigned int line);
   while (0)                                                                    \
   UTEST_SURPRESS_WARNING_END
 
+#define ASSERT_TRUE_(x, s) \
+  do {                      \
+    (tdbg << (s));          \
+    ASSERT_TRUE(x);        \
+  } while(0)
+
 #undef ASSERT_FALSE
 #define ASSERT_FALSE(x)                                                        \
   UTEST_SURPRESS_WARNING_BEGIN do {                                            \
@@ -109,6 +115,12 @@ void tdbg_clear_output(const char * file, unsigned int line);
   while (0)                                                                    \
   UTEST_SURPRESS_WARNING_END
 
+#define ASSERT_FALSE_(x, s) \
+  do {                      \
+    (tdbg << (s));          \
+    ASSERT_FALSE(x);        \
+  } while(0)
+
 #undef ASSERT_EQ
 #define ASSERT_EQ(x, y) UTEST_ASSERT(x, y, ==)
 #define ASSERT_EQ_(x, y, s) \
@@ -116,13 +128,43 @@ void tdbg_clear_output(const char * file, unsigned int line);
     (tdbg << (s));          \
     ASSERT_EQ(x, y);        \
   } while(0)
+
 #undef ASSERT_NE
 #define ASSERT_NE(x, y) UTEST_ASSERT(x, y, !=)
+#define ASSERT_NE_(x, y, s) \
+  do {                      \
+    (tdbg << (s));          \
+    ASSERT_NE(x, y);        \
+  } while(0)
+
 #undef ASSERT_LT
 #define ASSERT_LT(x, y) UTEST_ASSERT(x, y, <)
+#define ASSERT_LT_(x, y, s) \
+  do {                      \
+    (tdbg << (s));          \
+    ASSERT_LT(x, y);        \
+  } while(0)
+
 #undef ASSERT_LE
 #define ASSERT_LE(x, y) UTEST_ASSERT(x, y, <=)
+#define ASSERT_LE_(x, y, s) \
+  do {                      \
+    (tdbg << (s));          \
+    ASSERT_LE(x, y);        \
+  } while(0)
+
 #undef ASSERT_GT
 #define ASSERT_GT(x, y) UTEST_ASSERT(x, y, >)
+#define ASSERT_GT_(x, y, s) \
+  do {                      \
+    (tdbg << (s));          \
+    ASSERT_GT(x, y);        \
+  } while(0)
+
 #undef ASSERT_GE
 #define ASSERT_GE(x, y) UTEST_ASSERT(x, y, >=)
+#define ASSERT_GE_(x, y, s) \
+  do {                      \
+    (tdbg << (s));          \
+    ASSERT_GE(x, y);        \
+  } while(0)
