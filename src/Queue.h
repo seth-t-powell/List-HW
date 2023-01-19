007318a05cbd0d8e32b177c914924957b1426a8a
jdavstaff <jdavstaff@tamu.edu>
2023-01-18 23:35:46 -0600

    Add starter code for queue and stack

---
+++ src/Queue.h
@@ -0,0 +1,40 @@
+#include "List.h"
+
+template <typename T, typename Container = List<T>>
+class Queue {
+    public:
+        // Aliases for accessing data types outside of the class
+        using container_type  = Container;
+        using value_type      = Container::value_type;
+        using size_type       = Container::size_type;
+        using reference       = Container::reference;
+        using const_reference = Container::const_reference;
+
+    private:
+        Container c;
+
+    public:
+        // The constructors, destructor, and assignment operators are done for you
+        Queue() = default;
+        Queue(const Queue& other) = default;
+        Queue(Queue&& other) = default;
+        ~Queue() = default;
+        Queue& operator=(const Queue& other) = default;
+        Queue& operator=(Queue&& other) = default;
+
+        reference front() { /* TODO */ }
+        const_reference front() const { /* TODO */ }
+        reference back() { /* TODO */ }
+        const_reference back() const { /* TODO */ }
+
+        bool empty() const { /* TODO */ }
+        size_type size() const { /* TODO */ }
+
+        void push(const value_type& value) { /* TODO */ }
+        void push(value_type&& value) { /* TODO */ }
+        void pop() { /* TODO */ }
+        void swap(Queue& other) noexcept { /* TODO */ }
+};
+
+template <typename T, typename Container>
+bool operator==(const Queue<T, Container>& lhs, const Queue<T, Container>& rhs) { /* TODO */ }
